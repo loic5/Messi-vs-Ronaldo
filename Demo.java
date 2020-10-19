660a3d7016e7482f280750cbdb3d87edd68e8dca
Mehr14 <73111300+Mehr14@users.noreply.github.com>
2020-10-19 16:58:52 +0530

    Demo

---
+++ Demo.java
@@ -0,0 +1,17 @@
+public class Demo
+{
+   private int rollNum;
+
+
+   Demo(int rnum)
+   {
+      rollNum = rollNum+ rnum;
+   }
+  
+
+   public static void main(String args[])
+   {
+     
+      Demo obj = new Demo();
+   }
+}
